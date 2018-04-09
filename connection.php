a18f13cdfed2400ee32582b03626c7b37dde8147
mohamedebrahim96 <ebrahimm131@gmail.com>
2018-04-09 04:31:25 +0200

    add new features

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
  $dbhost = 'localhost';
  $dbusername = 'id2326085_ebrahimm131';
  $dbpassword = 'ebrahimm131';
- $dbname = 'id2326085_cimaclub';
+ $dbname =     'id2326085_cimaclub';
 
  $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
 
--- request.php
+++ request.php
@@ -1,24 +1,23 @@
 <?php
 include "connection.php";
-if(isset($_POST['post']))
-{
-    try {
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO request(movie_id,title)
-    VALUES (
-     '".$_POST["movie_id"]."'
-    ,'".$_POST["title"]."'
-    )";
-    $conn->exec($sql);
-    echo "New request created successfully";
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
 
-$conn = null;
-   
+
+$sthandler = $conn->prepare("SELECT movie_id FROM `request` WHERE movie_id = :movie_id");
+$sthandler->bindParam(':movie_id', $_POST['movie_id']);
+$sthandler->execute();
+
+if($sthandler->rowCount() > 0){
+    echo "exists! cannot insert";
+} else {
+            $sql = "INSERT INTO `request` (`movie_id`, `title`) VALUES (:movie_id, :title)";
+            $statement = $conn->prepare($sql);
+            $statement->bindValue(':movie_id', $_POST['movie_id']);
+            $statement->bindValue(':title', $_POST['title']);
+            $inserted = $statement->execute();
+            if($inserted){
+                echo "Done!";
+            }                                           
 }
+
+
 ?>
