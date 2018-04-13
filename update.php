00d368ddded717d2555256551d6a3425b7d09069
mohamed ebrahim <ebrahimm131@gmail.com>
2018-04-13 05:32:58 +0200

    small changes

--- update.php
+++ update.php
@@ -2,10 +2,11 @@
 header('Content-Type: application/json');
 
 $foo = new StdClass();
-$foo->versioncode = 2;
+$foo->versioncode = 1;
 $foo->versionname = "1.0";
 $foo->title = "SOFTWARE UPDATE FAILED";
-$foo->message = "new update and add more featurs -fix bugs";
+$foo->message = "حمل التحديث يسطا وبلاش استعباط";
+$foo->download_link = "";
 
 
 $json = json_encode($foo);
