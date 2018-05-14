c9adc9829ba508aac83f1e9d430fb0604072c801
mohamed ebrahim <ebrahimm131@gmail.com>
2018-05-14 06:12:12 +0200

    new get file id chenge

--- getfileid.php
+++ getfileid.php
@@ -5,9 +5,14 @@ header('Content-Type: application/json');
 $statement = $conn->prepare("SELECT file_id FROM movies WHERE id= '".$_GET['id']."' "); 
 $statement->execute(); 
 
-//$results=$statement->fetchAll(PDO::FETCH_ASSOC);
 
 $row = $statement->fetch();
-echo "{$row['file_id']}";
+if($row<1){
+    echo "null";
+
+}else{
+    echo "{$row['file_id']}";
+
+}
 
 ?>
